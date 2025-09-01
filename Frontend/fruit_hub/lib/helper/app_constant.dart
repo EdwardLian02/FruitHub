7a5e8f2b851547289a3fa70503cd437946392b73
Edward Lian <aungbobohein382@gmail.com>
2025-09-01 22:05:31 +0630

    setup

---
+++ Frontend/fruit_hub/lib/helper/app_constant.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyColor {
+  static const Color primaryColor = Color(0xFFFFA451);
+  static const Color lowGreyColor = Color(0xFFF3F1F1);
+  static const Color darkenGeryColor = Color(0xFFC2BDBD);
+
+  static const Color primaryTextColor = Colors.black;
+  static const Color secondaryTextColor = Color(0xFFFFA451);
+  static const Color whiteTextColor = Colors.white;
+}
+
+class FontTheme {
+  //font family
+  static const fontTheme = GoogleFonts.roboto;
+
+  //Font size
+  static const double textSizeExtraXLLarge = 40;
+  static const double textSizeExtraXLarge = 30;
+
+  static const double textSizeExtraLarge = 24;
+  static const double textSizeLarge = 20;
+  static const double textSizeNormal = 16;
+  static const double textSizeSmall = 14;
+}
+
+class BaseUrl {}
